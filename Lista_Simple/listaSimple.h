16b43c59478b7e1a8660f9a91d64c26e6bc03e9e
NoeSoto <55300210+NoeSoto@users.noreply.github.com>
2019-11-30 23:58:17 -0600

    Update listaSimple.h

--- Lista_Simple/listaSimple.h
+++ Lista_Simple/listaSimple.h
@@ -9,16 +9,17 @@ struct ElementoLista {
   struct ElementoLista *siguiente;
 };
 
-struct ListaId {
-  struct ElementoLista *inicio;
-  struct ElementoLista *fin;
-  int tamano;
+struct Lista {
+  int dato;
+  struct Lista *siguiente;
+  struct Lista *actual;
+  int tamano
 };
 
-void inicializacion(struct ListaId *lista);
-int ins_lista_vacia(struct ListaId *lista, int dato);
-int ins_lista(struct ListaId *lista, int dato);
-void visualizar(struct ListaId *lista);
-int eliminar(struct ListaId *lista, int pos);
+void inicializacion(struct Lista *lista);
+int ins_lista_vacia(struct Lista *lista, int dato);
+int ins_lista(struct Lista *lista, int dato);
+void visualizar(struct Lista *lista);
+int eliminar(struct Lista *lista, int pos);
 
 #endif
