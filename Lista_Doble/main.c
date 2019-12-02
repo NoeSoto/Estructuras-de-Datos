9730707f85dc717bcc137a06b003ea8a4a9a104e
NoeSoto <55300210+NoeSoto@users.noreply.github.com>
2019-12-01 23:18:02 -0600

    Update main.c

--- Lista_Doble/main.c
+++ Lista_Doble/main.c
@@ -1,20 +1,24 @@
 #include "listaDoble.h"
-int main(){
-    int opcion, pos, i;
-    char dato;
-    struct ListaId *lista;
-    struct ElementoLista *prueba;
-    //Inicializacion
-    if((lista=(struct ListaId *)malloc(sizeof(struct ListaId)))==NULL)
-        return -1;
-    ListaDoble_inicializacion(lista);
 
-    ListaDoble_insercion_en_lista_vacia(lista, 0);
+int
+main ()
+{
+  int opcion, pos, i;
+  char dato;
+  struct ListaDob *lista;
+  struct ListaDob *prueba;
+  //Inicializacion
+  if ((lista = (struct ListaDob *) malloc (sizeof (struct ListaDob))) == NULL)
+    return -1;
+  ListaDoble_inicializacion (lista);
 
-    for(i=1;i<=6;i++){
-ListaDoble_ins_fin_lista(lista, i);
-}
-ListaDoble_eliminar(lista, 3);
-ListaDoble_visualiza_adelante(lista);
-return 0;
+  ListaDoble_insercion_en_lista_vacia (lista, 0);
+
+  for (i = 1; i <= 6; i++)
+    {
+      ListaDoble_ins_fin_lista (lista, i);
+    }
+  ListaDoble_eliminar (lista, 3);
+  ListaDoble_visualiza_adelante (lista);
+  return 0;
 }
