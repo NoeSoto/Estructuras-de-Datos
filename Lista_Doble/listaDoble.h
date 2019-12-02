0a78adf8b6721f1f57c6d2ee475c72f0fdf4c60e
NoeSoto <55300210+NoeSoto@users.noreply.github.com>
2019-12-01 21:03:44 -0600

    Update listaDoble.h

--- Lista_Doble/listaDoble.h
+++ Lista_Doble/listaDoble.h
@@ -5,34 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct ElementoLista
+struct ListaDob
 {
-  int dato;
-  struct ElementoLista *anterior;
-  struct ElementoLista *siguiente;
-};
-
-struct ListaId
-{
-		struct ElementoLista *inicio;
-		struct ElementoLista *fin;
+		int dato;
+		struct ListaDob *inicio;
+		struct ListaDob *fin;
+		struct ListaDob *siguiente;
+		struct ListaDob *anterior;
 		int tamano;
 };
 
-void ListaDoble_inicializacion(struct ListaId *lista);
-int ListaDoble_insercion_en_lista_vacia(struct ListaId *lista, int dato);
-int ListaDoble_ins_inicio_lista(struct ListaId *lista, int dato);
-int ListaDoble_ins_fin_lista(struct ListaId *lista, int dato);
-int ListaDoble_ins_antes(struct ListaId *lista, int dato, int pos);
-int ListaDoble_ins_despues(struct ListaId *lista, int dato, int pos);
-void ListaDoble_visualiza_adelante(struct ListaId *lista);
-void ListaDoble_visualiza_atras(struct ListaId *lista);
-int ListaDoble_eliminar(struct ListaId *lista, int pos);
-char ListaDoble_ver_caracter(struct ListaId *lista, int pos);
-//void ListaDoble_destruir(ListaDoble *lista);// Debera implementar esta funcion
-//int ListaDoble_sup_inicio(ListaDoble * lista);
-//int ListaDoble_sup_en_lista(ListaDoble * lista, int pos);
-//int ListaDoble_ins_lista(ListaDoble *lista, char *dato,int pos);
+void ListaDoble_inicializacion(struct ListaDob *lista);
+int ListaDoble_insercion_en_lista_vacia(struct ListaDob *lista, int dato);
+int ListaDoble_ins_inicio_lista(struct ListaDob *lista, int dato);
+int ListaDoble_ins_fin_lista(struct ListaDob *lista, int dato);
+int ListaDoble_ins_antes(struct ListaDob *lista, int dato, int pos);
+int ListaDoble_ins_despues(struct ListaDob *lista, int dato, int pos);
+void ListaDoble_visualiza_adelante(struct ListaDob *lista);
+void ListaDoble_visualiza_atras(struct ListaDob *lista);
+int ListaDoble_eliminar(struct ListaDob *lista, int pos);
+char ListaDoble_ver_caracter(struct ListaDob *lista, int pos);
 
 
 #endif
