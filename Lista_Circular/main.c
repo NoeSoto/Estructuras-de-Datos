5887278b8314b1fd498af5a614868e0bb1d24349
NoeSoto <55300210+NoeSoto@users.noreply.github.com>
2019-12-02 23:40:41 -0600

    Update main.c

--- Lista_Circular/main.c
+++ Lista_Circular/main.c
@@ -1,20 +1,22 @@
 #include "listaCircular.h"
 
-int main(){
-        int opcion, pos,i;
-        int dato;
-        struct ListaId *lista;
-        struct ElementoLista *prueba;
-        if((lista = (struct ListaId *)malloc(sizeof(struct ListaId)))==NULL)
-          return -1;
-        inicializacion(lista);
-        ins_lista_vacia(lista, 0);
-       // visualizar(lista);
-        for(i=1;i<=7;i++)
-         ins_lista(lista, i);
+int
+main ()
+{
+  int opcion, pos, i;
+  int dato;
+  struct Listacir *lista;
+  struct Listacir *prueba;
+  if ((lista = (struct Listacir *) malloc (sizeof (struct Listacir))) == NULL)
+    return -1;
+  inicializacion (lista);
+  ins_lista_vacia (lista, 0);
+  // visualizar(lista);
+  for (i = 1; i <= 7; i++)
+    ins_lista (lista, i);
 
-        visualizar(lista);
-       eliminar(lista,3);
-       visualizar(lista);
-        return 0;
+  visualizar (lista);
+  eliminar (lista, 3);
+  visualizar (lista);
+  return 0;
 }
