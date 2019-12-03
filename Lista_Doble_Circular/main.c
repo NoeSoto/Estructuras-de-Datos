678f61f35f37d92edae3106721cae20194344338
NoeSoto <55300210+NoeSoto@users.noreply.github.com>
2019-12-02 22:23:13 -0600

    Update main.c

--- Lista_Doble_Circular/main.c
+++ Lista_Doble_Circular/main.c
@@ -1,21 +1,24 @@
 #include "ListaDobcir.h"
-int main(){
-    int opcion, pos, i;
-    char dato;
-    struct CircularId *lista;
-    struct ElementoCircular *prueba;
-    //Inicializacion
-    if((lista=(struct CircularId *)malloc(sizeof(struct CircularId)))==NULL)
-        return -1;
-    inicializacion(lista);
+int
+main ()
+{
+  int opcion, pos, i;
+  char dato;
+  struct Circular *lista;
+  struct Circular *prueba;
+  //Inicializacion
+  if ((lista = (struct Circular *) malloc (sizeof (struct Circular))) == NULL)
+    return -1;
+  inicializacion (lista);
 
-    insercion_lista_vacia(lista, 0);
+  insercion_lista_vacia (lista, 0);
 
-    for(i=1;i<=6;i++){
-    insercion_fin(lista, i);
-}
-visualizar_adelante(lista);
-eliminar(lista, 3);
-visualizar_adelante(lista);
-return 0;
+  for (i = 1; i <= 6; i++)
+    {
+      insercion_fin (lista, i);
+    }
+  visualizar_adelante (lista);
+  eliminar (lista, 3);
+  visualizar_adelante (lista);
+  return 0;
 }
