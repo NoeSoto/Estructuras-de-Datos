03bd6f45286e9a5612d1580552c1cf99e3ee5d87
NoeSoto <55300210+NoeSoto@users.noreply.github.com>
2019-12-03 22:26:23 -0600

    Update main.c

--- Cola/main.c
+++ Cola/main.c
@@ -1,22 +1,25 @@
 #include "cola.h"
 
-int main(){
+int
+main ()
+{
 
-        int opcion, pos, i;
-        int dato;
-        struct ColaId *cola;
-        struct ElementoCola *prueba;
-        if((cola = (struct ColaId *)malloc(sizeof (struct ColaId)))==NULL)
-        return -1;
-        inicializacion(cola);
-        ins_cola_vacia(cola,0);
+  int opcion, pos, i;
+  int dato;
+  struct Cola *cola;
+  struct Cola *prueba;
+  if ((cola = (struct Cola *) malloc (sizeof (struct Cola))) == NULL)
+    return -1;
+  inicializacion (cola);
+  ins_cola_vacia (cola, 0);
 
-        for(i=1;i<=7;i++){
-           ins_cola(cola, i);
-        }
-        visualizar(cola);
-        eliminar(cola);
-        visualizar(cola);
+  for (i = 1; i <= 7; i++)
+    {
+      ins_cola (cola, i);
+    }
+  visualizar (cola);
+  eliminar (cola);
+  visualizar (cola);
 
-        return 0;
+  return 0;
 }
