cd5b9846133cc03cfe6084ab6a70b7d17195d635
NoeSoto <55300210+NoeSoto@users.noreply.github.com>
2019-12-03 21:58:55 -0600

    Update main.c

--- Pila/main.c
+++ Pila/main.c
@@ -1,22 +1,25 @@
 #include "pila.h"
 
-int main(){
+int
+main ()
+{
 
-        int opcion, i;
-        int dato;
-        struct PilaId *pila;
-        struct ElementoPila *prueba;
-        if((pila = (struct PilaId *)malloc(sizeof (struct PilaId)))==NULL)
-        return -1;
-        inicializacion(pila);
-        push_pila_vacia(pila,0);
+  int opcion, i;
+  int dato;
+  struct Pila *pila;
+  struct Pila *prueba;
+  if ((pila = (struct Pila *) malloc (sizeof (struct Pila))) == NULL)
+    return -1;
+  inicializacion (pila);
+  push_pila_vacia (pila, 0);
 
-        for(i=1;i<=7;i++){
-           push_pila(pila, i);
-        }
-        visualizar(pila);
-        pop(pila);
-        visualizar(pila);
+  for (i = 1; i <= 7; i++)
+    {
+      push_pila (pila, i);
+    }
+  visualizar (pila);
+  pop (pila);
+  visualizar (pila);
 
-        return 0;
+  return 0;
 }
